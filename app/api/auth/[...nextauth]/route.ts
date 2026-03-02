b44ac180b6c5231b515279a03fb793eebff9a420
Joan Gerard <joan.gerard@outlook.com>
2026-03-02 03:32:03 +0100

    remove logs

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,20 +11,13 @@ const handler = NextAuth({
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        console.log("ENV EMAIL:", process.env.ADMIN_EMAIL);
-        console.log("ENV HASH:", process.env.ADMIN_PASSWORD_HASH);
-        console.log("SUBMITTED EMAIL:", credentials?.email);
-
         if (!credentials?.email || !credentials?.password) return null;
 
         const isValidEmail = credentials.email === process.env.ADMIN_EMAIL;
-        console.log("EMAIL MATCH:", isValidEmail);
         const isValidPassword = await bcrypt.compare(
           credentials.password,
           process.env.ADMIN_PASSWORD_HASH!
         );
-        console.log("PASSWORD:", credentials.password);
-        console.log("PASSWORD MATCH:", isValidPassword);
 
         if (!isValidEmail || !isValidPassword) return null;
 
